ee57f860a89adea14a0632fc8b88c221cc9921d1
0xRaccoon <raccoon@defi.sucks>
2023-11-27 15:46:51 +0100

    feat: add createdAt data for BatchRequestsForFinalizeData and BatchDisputesData
    Signed-off-by: 0xRaccoon <raccoon@defi.sucks>

--- contracts/BatchRequestsForFinalizeData.sol
+++ contracts/BatchRequestsForFinalizeData.sol
@@ -8,10 +8,16 @@ import {IOracle} from '@defi-wonderland/prophet-core-abi/solidity/interfaces/IOr
  * @notice This contract is used to get batch requests min data from the oracle contract to finalize the requests
  */
 contract BatchRequestsForFinalizeData {
+  struct ResponseData {
+    bytes32 responseId;
+    uint256 responseCreatedAt;
+  }
+
   struct RequestForFinalizeData {
     bytes32 requestId;
+    uint256 requestCreatedAt;
     uint256 finalizedAt;
-    bytes32[] responseIds;
+    ResponseData[] responses;
   }
 
   constructor(IOracle _oracle, uint256 _startFrom, uint256 _amount) {
@@ -22,9 +28,19 @@ contract BatchRequestsForFinalizeData {
     for (uint256 _i = 0; _i < _requestsIds.length; _i++) {
       bytes32 _requestId = _requestsIds[_i];
 
-      bytes32[] memory _responses = _oracle.getResponseIds(_requestId);
+      bytes32[] memory _responsesIds = _oracle.getResponseIds(_requestId);
+      ResponseData[] memory _responses = new ResponseData[](_responsesIds.length);
 
-      _returnData[_i] = RequestForFinalizeData({requestId: _requestId, finalizedAt: _oracle.finalizedAt(_requestId), responseIds: _responses});
+      for (uint256 _j = 0; _j < _responsesIds.length; _j++) {
+        _responses[_j] = ResponseData({responseId: _responsesIds[_j], responseCreatedAt: _oracle.createdAt(_responsesIds[_j])});
+      }
+
+      _returnData[_i] = RequestForFinalizeData({
+        requestId: _requestId,
+        requestCreatedAt: _oracle.createdAt(_requestId),
+        finalizedAt: _oracle.finalizedAt(_requestId),
+        responses: _responses
+      });
     }
 
     // encode return data
